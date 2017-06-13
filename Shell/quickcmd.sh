6bf72a5e3d69c2cb4ca4d0a13fd898fef52d2cd0
good <123@123.com>
2017-06-13 19:52:00 +0800

    Signed-off-by: good <123@123.com>

--- Shell/quickcmd.sh
+++ Shell/quickcmd.sh
@@ -1,6 +1,12 @@
 if [  $# -eq 0 ]
 then
 	cat /usr/bin/cmd.txt |awk '{print "-----"  NR " " $0 }' 
+	cp /usr/bin/cmd.txt /usr/bin/cmdbk.txt
 else
- 	cat /usr/bin/cmd.txt |awk '{if(NR=="'$1'") {print $0}}' >tmp.sh && chmod +x tmp.sh && cat  tmp.sh && ./tmp.sh
+	if [ $1 == "add" ]
+	then
+		echo $2>>/usr/bin/cmd.txt
+	else	
+ 		cat /usr/bin/cmd.txt |awk '{if(NR=="'$1'") {print $0}}' >/tmp/tmp.sh && chmod +x /tmp/tmp.sh && cat  /tmp/tmp.sh && /tmp/tmp.sh
+	fi
 fi
