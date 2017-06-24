e795bf297ceaf0fc159fce5a438d24104cf33f65
good <1028199089@123.com>
2017-06-24 19:08:00 +0800

    添加执行以及打印常用命令
    Signed-off-by: good <1028199089@123.com>

--- Shell/quick_exec_cmd/quickcmd.sh
+++ Shell/quick_exec_cmd/quickcmd.sh
@@ -6,7 +6,15 @@ else
 	if [ $1 == "add" ]
 	then
 		echo $2>>/usr/bin/cmd.txt
-	else	
- 		cat /usr/bin/cmd.txt |awk '{if(NR=="'$1'") {print $0}}' >/tmp/tmp.sh && chmod +x /tmp/tmp.sh && cat  /tmp/tmp.sh && /tmp/tmp.sh
+	elif [ $1 == "del" ]
+	then 
+ 		cat /usr/bin/cmd.txt |awk '{if(NR!="'$2'") {print $0}}' >/tmp/tmp.txt && cp /tmp/tmp.txt /usr/bin/cmd.txt
+	elif [ $1 == "exec" ]
+	then
+ 		cat /usr/bin/cmd.txt |awk '{if(NR=="'$2'") {print $0}}' >/tmp/tmp.sh && chmod +x /tmp/tmp.sh && cat  /tmp/tmp.sh && /tmp/tmp.sh
+	elif [ $1 == "show" ]
+	then
+		outputdst=`tty`
+# 		sleep 1&& cat /usr/bin/cmd.txt |awk '{if(NR=="'$2'") {printf $0}}' |xargs > $outputdst &
 	fi
 fi
