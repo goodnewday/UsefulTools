5f1c524303a079702f073eabad1ff042d1a04154
good <123@123.com>
2017-06-24 15:43:04 +0800

    Windows Batch(批处理),一些常用操作可以使用批处理完成,简化重复工作,例如常见的文件拷贝等

---
+++ WindowsBatch/compile.bat
@@ -0,0 +1,16 @@
+COPY D:\svn\SDL2-2.0.5\VisualC\Debug\quicklog.dll		D:\svn\SDL2-2.0.5
+
+COPY D:\svn\SDL2-2.0.5\VisualC\Debug\quicklog.lib		D:\svn\SDL2-2.0.5
+
+COPY D:\svn\SDL2-2.0.5\VisualC\Debug\quicklog.pdb		D:\svn\SDL2-2.0.5
+
+
+COPY D:\svn\SDL2-2.0.5\VisualC\Win32\Debug\SDL2.dll     D:\svn\AVXPlayer\Debug
+COPY D:\svn\SDL2-2.0.5\VisualC\Win32\Debug\SDL2.lib 	D:\svn\AVXPlayer\Debug
+COPY D:\svn\SDL2-2.0.5\VisualC\Win32\Debug\SDL2.pdb     D:\svn\AVXPlayer\Debug
+
+COPY D:\svn\SDL2-2.0.5\VisualC\Win32\Debug\SDL2.lib     D:\svn\AVXPlayer\AVXPlayer\lib\win32\lib
+
+
+
+COPY D:\svn\SDL2-2.0.5\VisualC\Debug\quicklog.dll       D:\svn\AVXPlayer\Debug
